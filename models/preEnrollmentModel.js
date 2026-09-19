fbf93db0b38e2953586adbf16460a47fcdd6ded4
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-09-20 01:43:43 +0800

    preEnrollmentRejectReasons2

--- models/preEnrollmentModel.js
+++ models/preEnrollmentModel.js
@@ -356,7 +356,7 @@ export const bulkApproveApplicants = async (data) => {
 export const getRejectionReasons = async () => {
   return await db
     .selectFrom("rejection")
-    .selectAll()
+    .selectAll("rejection_reason_id", "rejection_reason")
     .orderBy("rejection_reason_id", "asc")
     .execute();
 };
