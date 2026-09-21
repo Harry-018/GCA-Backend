5d20d05f2ad8adc021e9bd4a05a96f469a255869
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-09-21 14:34:45 +0800

    preenrollmentcontrollererror3

--- controllers/preEnrollmentController.js
+++ controllers/preEnrollmentController.js
@@ -157,12 +157,18 @@ export const rejectApplicant = async (req, res) => {
 export const getApprovedApplicants = async (req, res) => {
   try {
     const approved = await pe.getApprovedApplicants(req.query.sub_date);
-    res.status(200).json({ data: approved });
-  } catch (error) {
-    console.error("GET APPROVED APPLICANTS ERROR:");
-    console.error(error);
 
-    res.status(500).json({ message: error.message });
+    res.status(200).json({
+      data: approved,
+    });
+  } catch (error) {
+    console.error("getApprovedApplicants error:", error);
+
+    res.status(500).json({
+      message: error.message,
+      code: error.code,
+      sqlMessage: error.sqlMessage,
+    });
   }
 };
 export const enrollApplicant = async (req, res) => {
