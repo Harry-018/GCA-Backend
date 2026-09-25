efe655633e8883f3170bf3624040589da38bc037
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-09-25 20:52:34 +0800

    teachers page test2

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getTeachers } from "../controllers/teachersController";
+
+import { getTeachers } from "../controllers/teachersController.js";
 import { authenticate, authorize } from "../middleware/authMiddleware";
 
 const router = express.Router();
